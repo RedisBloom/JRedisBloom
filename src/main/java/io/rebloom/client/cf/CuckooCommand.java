7a03ed14181a3e8f56a29b57b7939eca023eaa3d
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2020-12-17 21:50:30 -0700

    feat: enum (Jedis ProtocolCommands) for Cuckoo Filter commands

---
+++ src/main/java/io/rebloom/client/cf/CuckooCommand.java
@@ -0,0 +1,28 @@
+package io.rebloom.client.cf;
+
+import redis.clients.jedis.commands.ProtocolCommand;
+import redis.clients.jedis.util.SafeEncoder;
+
+public enum CuckooCommand implements ProtocolCommand {
+  RESERVE("CF.RESERVE"), //
+  ADD("CF.ADD"), //
+  ADDNX("CF.ADDNX"), //
+  INSERT("CF.INSERT"), //
+  INSERTNX("CF.INSERTNX"), //
+  EXIST("CF.EXISTS"), //
+  DEL("CF.DEL"), //
+  COUNT("CF.COUNT"), //
+  SCANDUMP("CF.SCANDUMP"), //
+  LOADCHUNK("CF.LOADCHUNK"), //
+  INFO("CF.INFO");
+
+  private final byte[] raw;
+
+  CuckooCommand(String alt) {
+    raw = SafeEncoder.encode(alt);
+  }
+
+  public byte[] getRaw() {
+    return raw;
+  }
+}
